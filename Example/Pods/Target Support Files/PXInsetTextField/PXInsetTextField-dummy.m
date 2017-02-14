b8cbcf0862d746b1885f08c103bb3bfe43df890f
Michael Biggs <mbiggs@pixio.com>
2017-02-14 12:31:18 -0700

    Updated podfile to work with CocoaPods 1.0, and update project settings.

---
+++ Example/Pods/Target Support Files/PXInsetTextField/PXInsetTextField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PXInsetTextField : NSObject
+@end
+@implementation PodsDummy_PXInsetTextField
+@end
--- Example/Pods/Target Support Files/Pods-PXInsetTextField-PXInsetTextField/Pods-PXInsetTextField-PXInsetTextField-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PXInsetTextField_PXInsetTextField : NSObject
-@end
-@implementation PodsDummy_Pods_PXInsetTextField_PXInsetTextField
-@end
